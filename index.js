9973d3e065f030e65f5d8ef514d432620fde1429
santiago ramirez cano <santiago.ramirezc26@gmail.com>
2022-11-07 17:07:57 -0500

    Created a basic express server with graphql and graphiql, using course schema and resolvers on code

---
+++ index.js
@@ -0,0 +1,30 @@
+'use strict'
+
+const { buildSchema } = require('graphql')
+const express = require('express')
+const { graphqlHTTP } = require('express-graphql')
+const { readFileSync } = require('fs')
+const { join } = require('path')
+const resolvers = require('./lib/resolvers')
+
+const app = express()
+const port = process.env.port || 3000
+
+// defining the schema
+const schema = buildSchema(
+    readFileSync(
+        join(__dirname, 'lib', 'schema.graphql'),
+        'utf-8'
+    )
+)
+
+
+app.use('/api', graphqlHTTP({
+  schema,
+  rootValue: resolvers,
+  graphiql: true
+}))
+
+app.listen(port, () => {
+  console.log(`Server is listening at http://localhost:${port}/api`)
+})
